dcaac76a45c2f6fb3dec5f64b163d10ac5ba7feb
D:/Cypress/Orange HRM <etumahmuda@gmail.com>
2023-06-01 14:39:24 +0600

    select item

--- cypress/e2e/MGM/login.cy.js
+++ cypress/e2e/MGM/login.cy.js
@@ -1,17 +1,17 @@
 describe("login ",()=>{
     it("should be login  with valid info",()=>{
-        // cy.login("01928374657","123456");
-        // cy.visit("https://devmgm.ibos.io/purchase/goodsReceive");
-        // cy.get("#demo-positioned-button").eq(0).click({force:true});
-        // cy.get("#supplier > div")
-        // .click({ force: true })
-        // .get("#react-select-mgm-option-4")
-        // .type("{enter}",{force: true});
-        // cy.wait(2000);
-        // cy.get(".css-b62m3t-container > div").eq(7)
-        // .click({ force: true }).debug()
-        // .get(".css-abc123-option-5")
-        // .click({ force: true });
+        cy.login("01928374657","123456");
+        cy.visit("https://devmgm.ibos.io/purchase/goodsReceive");
+        cy.get("#demo-positioned-button").eq(0).click({force:true});
+        cy.get("#supplier > div")
+        .click({ force: true })
+        .get("#react-select-mgm-option-4")
+        .type("{enter}",{force: true});
+        cy.wait(2000);
+        cy.get("#item > div")
+        .click({ force: true })
+        .get("#react-select-mgm-option-2")
+        .type("{enter}",{force: true});
         // cy.get('.css-b62m3t-container > div')
         // .eq(7)
         // .click({force:true})
@@ -24,9 +24,9 @@ describe("login ",()=>{
         //  .should('have.attr', 'aria-expanded', 'true')
     });
 
-    it("should be Create a Bank Account,view details & close ",()=>{
-     cy.login("01928374657","123456");
-     cy.createBankAccount("Ameena2","0131565561812","NRI001");
-    })
+    // it("should be Create a Bank Account,view details & close ",()=>{
+    //  cy.login("01928374657","123456");
+    //  cy.createBankAccount("Ameena2","0131565561812","NRI001");
+    // })
 
 });
